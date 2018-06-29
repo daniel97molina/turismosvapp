e7a19ac03a152ff0ed6906721965bdd55ed51c61
Irvin Ayala <hypleyone@gmail.com>
2018-06-29 12:40:49 -0600

    Mapa funcional mediante intent

--- app/src/main/java/turismo/pam/ues/turismosv/MapsActivity.java
+++ app/src/main/java/turismo/pam/ues/turismosv/MapsActivity.java
@@ -3,6 +3,7 @@ package turismo.pam.ues.turismosv;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -43,8 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         double longitud=intent.getDoubleExtra("longitud",-89.561878);
         String nombre=intent.getStringExtra("nombre");
         LatLng place = new LatLng(latitud,longitud);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         mMap.addMarker(new MarkerOptions().position(place).title(nombre));
         // Add a marker in Sydney and move the camera
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place,20f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place,17f));
     }
 }
